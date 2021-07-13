320c5df2bdea280146900705a6153a112e84081b
chinny36 <brownj95@gmail.com>
2021-07-13 18:22:43 -0400

    fetch todos from database

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -1,15 +1,23 @@
+const { db } = require("../util/admin");
+
 exports.getAllTodos = (req, res) => {
-  todos = [
-    {
-      id: "1",
-      title: "greeting",
-      body: "Hello world"
-    },
-    {
-      id: "2",
-      title: "greeting2",
-      body: "Hello2 world2"
-    }
-  ];
-  return res.json(todos);
+  db.collection("todos")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((data) => {
+      let todos = [];
+      data.forEach((doc) => {
+        todos.push({
+          todoId: doc.id,
+          title: doc.data().title,
+          body: doc.data().body,
+          createdAt: doc.data().createdAt
+        });
+      });
+      return res.json(todos);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
 };
--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const functions = require("firebase-functions");
-const app = require("express");
+const app = require("express")();
 const { getAllTodos } = require("./APIs/todos");
 
 app.get("/todos", getAllTodos);
